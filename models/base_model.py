a4c597cd8dd03c782733d70843ffb034fcd30056
root <chasenaidoo9@gmail.com>
2023-11-19 07:10:37 -0800

    Modified basemodel for metadata

--- models/base_model.py
+++ models/base_model.py
@@ -10,39 +10,46 @@ Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    __tablename__ = 'base_model'
+
+    metadata = Base.metadata
+
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-            
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
+        from models import storage
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key not in ['__class__', 'created_at', 'updated_at']:
                     setattr(self, key, value)
-            
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.utcnow()
+
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.utcnow()
+
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        cls = type(self).__name__
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """Updates updated_at with the current time when the instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         storage.save()
 
     def to_dict(self):
