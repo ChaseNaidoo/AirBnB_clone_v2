833436841c8ae5e2510cc86a8eb9262c7e5d6f3c
root <chasenaidoo9@gmail.com>
2023-11-22 05:48:17 -0800

    Changed permissions on python files

--- models/city.py
+++ models/city.py
@@ -6,11 +6,10 @@ from sqlalchemy.orm import relationship
 from models.place import Place
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
-    places = relationship("Place", back_populates="city", cascade="all, delete-orphan")
+    places = relationship("Place", backref="city", cascade="all, delete-orphan")
