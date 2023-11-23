1896f0907a4bb881b4f7ad0f7964e13bfab55716
Kcyberh <Kcyberh30@gmail.com>
2023-11-23 03:00:05 -0800

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, Table, ForeignKey
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    """ Amenity class """
-    __tablename__ = 'amenities'
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("PlaceAmenity", back_populates="amenity")
+    place_amenities = relationship("Place", secondary=place_amenity)
