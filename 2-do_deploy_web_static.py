c769caec5ee96b459f63b41e18e47138a93461c4
chasenaidoo <chasenaidoo9@gmail.com>
2023-12-06 10:44:41 -0800

    Distribute archived files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,3 +25,5 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(folder_name))
         return True
+    except Exception as e:
+        return False
