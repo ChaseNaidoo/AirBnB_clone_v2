dd37bb049f0e74bea6d7aa8a6bcd45ba2060ef0d
chasenaidoo <chasenaidoo9@gmail.com>
2023-12-06 03:16:43 -0800

    Set up web server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+nginx_config="/etc/nginx/sites-available/default"
+sudo sed -i "/server {/a\
+    location /hbnb_static/ {\
+        alias /data/web_static/current/;\
+    }" "$nginx_config"
+sudo service nginx restart
