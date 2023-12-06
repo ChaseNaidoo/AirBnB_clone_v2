9a73e27c33a4982fe4616e67bb5c97a1029a1cd7
chasenaidoo <chasenaidoo9@gmail.com>
2023-12-06 08:29:13 -0800

    Setup web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,14 +6,16 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "Holberton School" > /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
-nginx_config="/etc/nginx/sites-available/default"
-sudo sed -i "/server {/a\
-    location /hbnb_static/ {\
-        alias /data/web_static/current/;\
-    }" "$nginx_config"
+echo "server {
+    location /hbnb_static {
+            alias /data/web_static/current/;
+            index index.htm index.html;
+    }
+}" > /etc/nginx/sites-available/default
 sudo service nginx restart
+exit 0
