c73abd6cc86bdcf8e56aed75265c2e37ea7d8a45
chasenaidoo <chasenaidoo9@gmail.com>
2023-12-07 04:17:22 -0800

    Setup puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,52 +1,45 @@
 # Manifest for setting up web servers for the deployment of web_static
 
-# Update package repositories
-package { 'nginx':
-  ensure => 'installed',
-}
+# Update configuration
+$nginx_conf = "server {
+    location /hbnb_static {
+            alias /data/web_static/current/;
+            index index.htm index.html;
+    }
+}"
 
-# Create directories
-file { '/data/web_static/releases/test/':
-  ensure => 'directory',
-}
+package { 'nginx':
+    ensure => 'latest',
+    provider => 'apt'
+} ->
 
 file { '/data/web_static/shared/':
-  ensure => 'directory',
-}
+    ensure => 'directory',
+} ->
 
-# Create index.html
 file { '/data/web_static/releases/test/index.html':
-  content => 'Holberton School',
-}
+    ensure  => 'file',
+    content => 'Holberton School',
+} ->
 
-# Create symbolic link
 file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test/',
-  require => File['/data/web_static/releases/test/'],
-}
+    ensure => 'link',
+    target => '/data/web_static/releases/test/',
+    force  => true,
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+} ->
 
-# Set ownership
-file { '/data/':
-  recurse => true,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-}
-
-# Configure Nginx
 file { '/etc/nginx/sites-available/default':
-  content => "server {
-  location /hbnb_static {
-      alias /data/web_static/current/;
-      index index.htm index.html;
-  }
-}",
-  notify  => Service['nginx'],
-}
+    ensure  => 'file',
+    content => $nginx_conf,
+    require => File['/data/web_static/current'],
+    notify  => Service['nginx'],
+} ->
 
-# Restart Nginx
 service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+    ensure    => 'running',
+    enable    => true,
+    require   => File['/etc/nginx/sites-available/default'],
+    subscribe => File['/etc/nginx/sites-available/default'],
 }
