550fcf70994fe4718c4561156aac3d1b11165227
chasenaidoo <chasenaidoo9@gmail.com>
2023-12-24 04:09:19 -0800

    Modifed storage and state.py

--- models/state.py
+++ models/state.py
@@ -11,10 +11,21 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
 
     @property
     def get_cities(self):
-        """ Getter attribute that returns the list of City instances with state_id equals to the current State.id """
+        """ Getter attribute that returns
+        the list of City instances """
         from models import storage
-        return [city for city in storage.all(City).values() if city.state_id == self.id]
+        return [city for city in storage.all(City).values()
+                if city.state_id == self.id]
+
+    @property
+    def cities(self):
+        """ Getter attribute that returns the list of
+        City objects linked to the current State """
+        from models import storage
+        return [city for city in storage.all(City).values()
+                if city.state_id == self.id]
